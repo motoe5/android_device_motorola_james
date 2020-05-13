a9e4b7413fe7256b9bdd9f03ada51fc57ca0e023
Hyper <HyperServer@gmail.com>
2020-05-12 19:07:02 -0500

    james: TwerK it
    Signed-off-by: Hyper <HyperServer@gmail.com>

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,10 +2,10 @@
 
 SKU=`getprop ro.boot.hardware.sku`
 
-rm -rf /vendor/app/MotoActions
 mv /vendor/lib/egl/libEGL_adreno_8917.so /vendor/lib/egl/libEGL_adreno.so
 mv /vendor/lib/egl/libGLESv1_CM_adreno_8917.so /vendor/lib/egl/libGLESv1_CM_adreno.so
 mv /vendor/lib/egl/libGLESv2_adreno_8917.so /vendor/lib/egl/libGLESv2_adreno.so
 mv /vendor/lib64/egl/libEGL_adreno_8917.so /vendor/lib64/egl/libEGL_adreno.so
 mv /vendor/lib64/egl/libGLESv1_CM_adreno_8917.so /vendor/lib64/egl/libGLESv1_CM_adreno.so
 mv /vendor/lib64/egl/libGLESv2_adreno_8917.so /vendor/lib64/egl/libGLESv2_adreno.so
+rm -rf /*/app/*Actions
